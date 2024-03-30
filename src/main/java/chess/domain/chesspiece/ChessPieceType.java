55bbed6b8d7e11f9b5e71d4de39c6d889bcf7966
yoonseo choi <choiyoonseo02@gmail.com>
2024-03-30 16:23:00 +0900

    feat: 각 체스 말의 점수 추가

--- src/main/java/chess/domain/chesspiece/ChessPieceType.java
+++ src/main/java/chess/domain/chesspiece/ChessPieceType.java
@@ -8,22 +8,25 @@ import chess.domain.chesspiece.movestrategy.MoveStrategy;
 import chess.domain.chesspiece.movestrategy.PawnMoveStrategy;
 import chess.domain.chesspiece.movestrategy.QueenMoveStrategy;
 import chess.domain.chesspiece.movestrategy.RookMoveStrategy;
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 public enum ChessPieceType {
 
-    NONE(EmptyMoveStrategy::new),
-    KING(KingMoveStrategy::new),
-    QUEEN(QueenMoveStrategy::new),
-    BISHOP(BishopMoveStrategy::new),
-    KNIGHT(KnightMoveStrategy::new),
-    ROOK(RookMoveStrategy::new),
-    PAWN(PawnMoveStrategy::new);
+    NONE(EmptyMoveStrategy::new, 0),
+    KING(KingMoveStrategy::new, 0),
+    QUEEN(QueenMoveStrategy::new, 9),
+    BISHOP(BishopMoveStrategy::new, 3),
+    KNIGHT(KnightMoveStrategy::new, 2.5),
+    ROOK(RookMoveStrategy::new, 5),
+    PAWN(PawnMoveStrategy::new, 1);
 
     private final Supplier<MoveStrategy> moveStrategySupplier;
+    private final double score;
 
-    ChessPieceType(Supplier<MoveStrategy> moveStrategySupplier) {
+    ChessPieceType(Supplier<MoveStrategy> moveStrategySupplier, double score) {
         this.moveStrategySupplier = moveStrategySupplier;
+        this.score = score;
     }
 
     public static boolean isChessPieceTypeNone(ChessPieceType chessPieceType) {
