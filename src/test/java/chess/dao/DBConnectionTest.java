9b14c8f34982a761a1e49abb0c7b7c67540654bd
yoonseo choi <choiyoonseo02@gmail.com>
2024-03-31 22:48:18 +0900

    feat: DBConnection

---
+++ src/test/java/chess/dao/DBConnectionTest.java
@@ -0,0 +1,18 @@
+package chess.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class DBConnectionTest {
+
+    private final DBConnection dbConnection = new DBConnection();
+
+    @Test
+    public void connection() throws SQLException {
+        try (final var connection = dbConnection.getConnection()) {
+            assertThat(connection).isNotNull();
+        }
+    }
+}
